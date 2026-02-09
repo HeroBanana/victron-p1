cdb0a854cc372d9b5c4fd75a152bf7ba602124fe
Nick Vlug <nick@withblocks.com>
2026-02-09 12:18:41 +0100

    Make Modbus port configurable via add-on settings
    Port 502 may already be in use on the host. Added modbus_port option
    (default 502) so users can change it in the add-on configuration.

--- victron-p1/run.sh
+++ victron-p1/run.sh
@@ -3,10 +3,11 @@
 CONFIG_PATH=/data/options.json
 HOMEWIZARD_IP=$(jq -r '.homewizard_ip' $CONFIG_PATH)
 POLL_INTERVAL=$(jq -r '.poll_interval' $CONFIG_PATH)
+MODBUS_PORT=$(jq -r '.modbus_port' $CONFIG_PATH)
 
 export HOMEWIZARD_IP
 export POLL_INTERVAL
-export MODBUS_PORT=502
+export MODBUS_PORT
 export DEBUG=true
 
 exec node /app/index.js
